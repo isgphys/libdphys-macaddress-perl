cb8f289a9f9cc276748f55277b9ef95996473f4a
Elmar S. Heeb <elmar@heebs.ch>
2014-08-15 11:36:37 +0200

    split validation into public is_valid and private _to_binary

--- lib/DPHYS/MacAddress.pm
+++ lib/DPHYS/MacAddress.pm
@@ -6,16 +6,24 @@ package DPHYS::MacAddress;
 use Carp;
 
 use Moo;
+
+sub _to_binary {
+    my ($macaddr) = @_;
+    croak 'DPHYS::MacAddress::_to_binary is a class method only' if ref($macaddr) eq __PACKAGE__;
+
+    $macaddr =~ s{[-:\.]}{}xmsgi;
+    return unless $macaddr =~ m{\A [\d a-f]{12} \Z}xmsi;
+
+    return pack('H2' x 6, unpack('A2' x 6, $macaddr));
+}
+
 use namespace::clean;
 
 sub is_valid {
     my ($macaddr) = @_;
     croak 'DPHYS::MacAddress::is_valid is a class method only' if ref($macaddr) eq __PACKAGE__;
 
-    $macaddr =~ s{[-:\.]}{}xmsgi;
-    return 0 unless $macaddr =~ m{\A [\d a-f]{12} \Z}xmsi;
-
-    return 1;
+    return !! _to_binary($macaddr);
 }
 
 1;
